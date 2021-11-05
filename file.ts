a4d8da65f88336db6306b246833c2a251d9ca919
Olli Brummer <olli.brummer@knowit.fi>
2021-11-05 14:20:48 +0200

    add types

--- file.ts
+++ file.ts
@@ -1,1 +1,5 @@
-console.log("Hello world");
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+};
+
+multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
